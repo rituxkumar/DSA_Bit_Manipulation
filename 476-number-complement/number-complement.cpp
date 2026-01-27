2789314ebec4690df0cecad6e4beeb3186bbcbef
Ritu Kumar <the.ritukumar@gmail.com>
2026-01-27 20:14:00 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (44.71%) - LeetSync

---
+++ 476-number-complement/number-complement.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int numberInBits = (int)(log2(num)) + 1;
+
+        for (int i = 0; i < numberInBits; i++) {
+            num = num ^ (1 << i);
+        }
+        return num;
+    }
+};
