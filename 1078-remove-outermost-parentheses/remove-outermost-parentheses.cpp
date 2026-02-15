fc0194258ea55eb7628807a4b5d672701408f9a6
Ritu Kumar <the.ritukumar@gmail.com>
2026-02-15 15:03:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (54.16%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        stack<char> st;
+        string result = "";
+        for (char& c : s) {
+            if (c == '(') {
+                if (!st.empty()) 
+                    result += c;
+                    st.push(c);
+                } else {
+                    st.pop();
+                    if (!st.empty()) 
+                       result += c;
+                    }
+                }
+            
+        
+        return result;
+    }
+};
